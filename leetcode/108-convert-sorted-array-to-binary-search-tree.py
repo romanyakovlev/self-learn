c44615b7396da511d09c7daf51f5a5a1c142666d
romanyakovlev <yakovlevroman8@gmail.com>
2021-12-02 04:47:28 +0700

    108-convert-sorted-array-to-binary-search-tree

--- leetcode/108-convert-sorted-array-to-binary-search-tree.py
+++ leetcode/108-convert-sorted-array-to-binary-search-tree.py
@@ -21,5 +21,26 @@ class Solution:
         self.node.right = self.recurse(nums[i+1:])
         return self.node
         
+# better solution (withoug using slicing)
 
+# A lot of the Python solutions use slices to split the array; 
+# however, it takes O(n) to slice, making the entire algorithm O(n logn). 
+# Therefore, we create a helper function to pass in the bounds of the array instead, making it O(n):
+
+def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+	return self.helper(nums, 0, len(nums))
+
+def helper(self, nums, lower, upper):
+	if lower == upper:
+		return None
+
+	mid = (lower + upper) // 2
+	node = TreeNode(nums[mid])
+	node.left = self.helper(nums, lower, mid)
+	node.right = self.helper(nums, mid+1, upper)
+
+	return node
+
+# Please note the if lower == upper: return None statement -- since we are passing in bounds, 
+# nums will never be None. Therefore, we check if the lower and upper bounds are the same for our base case.
             

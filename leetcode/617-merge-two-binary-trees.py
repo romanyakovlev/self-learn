cc7facce131707b5f19b1fe6891f1fe0650ce960
Roman Yakovlev <yakovlevroman8@gmail.com>
2021-12-17 14:23:50 +0700

    Update 617-merge-two-binary-trees.py

--- leetcode/617-merge-two-binary-trees.py
+++ leetcode/617-merge-two-binary-trees.py
@@ -22,3 +22,14 @@ class Solution:
             return None
         self.recurse(root, root1, root2)
         return root
+
+# beautiful solution from disc
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        if t1 and t2:
+            root = TreeNode(t1.val + t2.val)
+            root.left = self.mergeTrees(t1.left, t2.left)
+            root.right = self.mergeTrees(t1.right, t2.right)
+            return root
+        else:
+            return t1 or t2

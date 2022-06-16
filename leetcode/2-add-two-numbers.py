8bfb032d52ab62c32c071111732dbb85f030e4aa
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-06-17 03:47:24 +0700

    Update 2-add-two-numbers.py

--- leetcode/2-add-two-numbers.py
+++ leetcode/2-add-two-numbers.py
@@ -4,6 +4,36 @@
 #         self.val = val
 #         self.next = next
 
+# Solution using recursion
+
+class Solution:
+    def recursion(self, end, l1, l2):
+        if l1 and l2:
+            s = l1.val + l2.val + end
+            if s >= 10:
+                return ListNode(val=s % 10, next=self.recursion(1, l1.next, l2.next))
+            else:
+                return ListNode(val=s, next=self.recursion(0, l1.next, l2.next))
+        elif l1:
+            s = l1.val + end
+            if s >= 10:
+                return ListNode(val=s % 10, next=self.recursion(1, l1.next, None))
+            else:
+                return ListNode(val=s, next=self.recursion(0, l1.next, None))
+        elif l2:
+            s = l2.val + end
+            if s >= 10:
+                return ListNode(val=s % 10, next=self.recursion(1, None, l2.next))
+            else:
+                return ListNode(val=s, next=self.recursion(0, None, l2.next))
+        elif end == 1:
+            return ListNode(val=end, next=self.recursion(0, None, None))
+        else:
+            return None
+    
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        return self.recursion(0, l1, l2)
+
 # Solution using iteration
 
 class Solution:

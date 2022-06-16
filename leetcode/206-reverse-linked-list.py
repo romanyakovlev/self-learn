9e88cfaa3e5b09b9b494e2c1c32be26ed19dc4c3
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-06-17 05:07:02 +0700

    Update 206-reverse-linked-list.py

--- leetcode/206-reverse-linked-list.py
+++ leetcode/206-reverse-linked-list.py
@@ -1,4 +1,23 @@
-# recursion approach
+# recursion approach (easier to understand)
+
+class Solution(object):
+    
+    def recurse(self, node, next_node):
+        if not node.next:
+            return ListNode(val=node.val, next=next_node)
+        return self.recurse(node.next, ListNode(val=node.val, next=next_node))
+        
+    def reverseList(self, head):
+        if not head:
+            return None
+        elif not head.next:
+            return ListNode(val=head.val, next=None)
+        elif not head.next.next:
+            return ListNode(val=head.next.val, next=ListNode(val=head.val, next=None))
+        else:
+            return self.recurse(head.next, ListNode(val=head.val, next=None))
+
+# recursion approach (wihout object creation)
 
 class Solution:
     def recurse(self, head):

d02f024e1399a56d8e04675708fed59a329a0150
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-06-28 21:37:02 +0700

    Update 1-two-sum.py

--- leetcode/1-two-sum.py
+++ leetcode/1-two-sum.py
@@ -14,7 +14,20 @@ class Solution:
             if y in d.keys() and len(d[y]):
                 y_i = d[y].pop()
                 return [x_i, y_i]
-                
+
+# second solution
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = dict()
+        for i, n in enumerate(nums):
+            v = target - n
+            if v in d.keys():
+                return [d[v], i]
+            else:
+                d[n] = i
+        
+
 # better solution
 
 class Solution:
@@ -26,3 +39,6 @@ class Solution:
                 d[x] = x_i
             else:
                 return x_i, d[y]
+            
+# good article about sums from leetcode (2sum, 3/4 sum, etc)
+# https://leetcode.com/problems/two-sum/discuss/737092/Sum-MegaPost-Python3-Solution-with-a-detailed-explanation

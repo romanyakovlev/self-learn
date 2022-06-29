51f98f05f5028bd10bd477c8ac82ee98c2ed8bb6
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-06-30 03:22:14 +0700

    Update 20-valid-parentheses.py

--- leetcode/20-valid-parentheses.py
+++ leetcode/20-valid-parentheses.py
@@ -17,8 +17,24 @@ class Solution:
                     l.pop()
             index += 1
         return True if len(l) == 0 else False
+  
+#second solution
+class Solution:
+    p_map = {'(': ')', '{': '}', '[': ']'}
+    
+    def isValid(self, s: str) -> bool:
+        l = []
+        for p in s:
+            if p in self.p_map.keys():
+                l.append(p)
+            elif l and self.p_map[l[-1]] == p:
+                l.pop()
+            else:
+                return False
+        return len(l) == 0
 
-# more beautiful
+
+# more beautiful - from leetcode discussion
 
 class Solution(object):
     def isValid(self, s):

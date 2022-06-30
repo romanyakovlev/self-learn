93fc160bed5f6bcb2f85313b292844ede96f1473
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-07-01 04:04:55 +0700

    Update 100-same-tree.py

--- leetcode/100-same-tree.py
+++ leetcode/100-same-tree.py
@@ -25,3 +25,16 @@ class Solution:
         elif p != q:
             self.same = False
         return self.same
+    
+# second solution
+class Solution:
+        
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p and q:
+            left = self.isSameTree(p.left, q.left)
+            right = self.isSameTree(p.right, q.right)
+            return p.val == q.val and left and right
+        elif (not q and p) or (q and not p):
+            return False
+        else:
+            return True

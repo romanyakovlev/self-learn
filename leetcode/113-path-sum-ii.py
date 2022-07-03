651ba207fdc68e46604e8229513c19210a749a86
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-07-04 04:04:07 +0700

    Update 113-path-sum-ii.py

--- leetcode/113-path-sum-ii.py
+++ leetcode/113-path-sum-ii.py
@@ -14,11 +14,8 @@ class Solution:
             l += [root.val]
             if s == self.target and not root.left and not root.right:
                 self.result_l.append(l)
-                return None
-            if root.left:
-                self.recurse(root.left, l[:], s)
-            if root.right:
-                self.recurse(root.right, l[:], s)
+            self.recurse(root.left, l[:], s)
+            self.recurse(root.right, l[:], s)
             
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         s = 0
@@ -27,4 +24,77 @@ class Solution:
         self.target = targetSum
         self.recurse(root, l[:], s)
         return self.result_l
+
+    
+ # solutions from leetcode
+    
+class Solution:
+    
+    def pathSum(self, root, sum):
+        res = []
+        self.dfs(root, sum, [], res)
+        return res
         
+    def dfs(self, root, sum, ls, res):
+        if root:
+			if not root.left and not root.right and sum == root.val:
+				ls.append(root.val)
+				res.append(ls)
+            self.dfs(root.left, sum-root.val, ls+[root.val], res)
+            self.dfs(root.right, sum-root.val, ls+[root.val], res)
+            
+    def pathSum2(self, root, sum):
+        if not root:
+            return []
+        if not root.left and not root.right and sum == root.val:
+            return [[root.val]]
+        tmp = self.pathSum(root.left, sum-root.val) + self.pathSum(root.right, sum-root.val)
+        return [[root.val]+i for i in tmp]
+    
+    # BFS + queue    
+    def pathSum3(self, root, sum): 
+        if not root:
+            return []
+        res = []
+        queue = [(root, root.val, [root.val])]
+        while queue:
+            curr, val, ls = queue.pop(0)
+            if not curr.left and not curr.right and val == sum:
+                res.append(ls)
+            if curr.left:
+                queue.append((curr.left, val+curr.left.val, ls+[curr.left.val]))
+            if curr.right:
+                queue.append((curr.right, val+curr.right.val, ls+[curr.right.val]))
+        return res
+        
+    # DFS + stack I  
+    def pathSum4(self, root, sum): 
+        if not root:
+            return []
+        res = []
+        stack = [(root, sum-root.val, [root.val])]
+        while stack:
+            curr, val, ls = stack.pop()
+            if not curr.left and not curr.right and val == 0:
+                res.append(ls)
+            if curr.right:
+                stack.append((curr.right, val-curr.right.val, ls+[curr.right.val]))
+            if curr.left:
+                stack.append((curr.left, val-curr.left.val, ls+[curr.left.val]))
+        return res 
+    
+    # DFS + stack II   
+    def pathSum5(self, root, s): 
+        if not root:
+            return []
+        res = []
+        stack = [(root, [root.val])]
+        while stack:
+            curr, ls = stack.pop()
+            if not curr.left and not curr.right and sum(ls) == s:
+                res.append(ls)
+            if curr.right:
+                stack.append((curr.right, ls+[curr.right.val]))
+            if curr.left:
+                stack.append((curr.left, ls+[curr.left.val]))
+        return res

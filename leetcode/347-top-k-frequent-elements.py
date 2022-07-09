69a1bd520a7bc0c84743392ff4e5af6627ab5eef
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-07-09 20:19:39 +0700

    Update 347-top-k-frequent-elements.py

--- leetcode/347-top-k-frequent-elements.py
+++ leetcode/347-top-k-frequent-elements.py
@@ -2,7 +2,6 @@
 # first solution
 
 class Solution:
-  
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         d = {}
         h, l = [], []
@@ -10,7 +9,4 @@ class Solution:
             d[n] = d.get(n, 0) + 1
         for n, c in d.items():
             heapq.heappush(h, (-c, n))
-        while True:
-            l.append(heapq.heappop(h)[1])
-            if len(l) == k:
-                return l
+        return [heapq.heappop(h)[1] for _ in range(k)]

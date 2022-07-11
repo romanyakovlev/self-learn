a13a944f27f3786fc9af2d67110bddf5938f04db
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-07-12 03:58:42 +0700

    Update 200-number-of-islands.py

--- leetcode/200-number-of-islands.py
+++ leetcode/200-number-of-islands.py
@@ -1,33 +1,33 @@
 # first solution
 
 class Solution:
-    def fill_land(self, grid, i, j):
-        if grid[i][j] == "1" and self.check_grid[i][j] == 0:
+    def fill_land(self, i, j):
+        if self.grid[i][j] == "1" and self.check_grid[i][j] == 0:
             self.check_grid[i][j] = 1
         else:
             return None
         if j - 1 >= 0:
-            self.fill_land(grid, i, j - 1)
-        if j + 1 < len(grid[0]):
-            self.fill_land(grid, i, j + 1)
+            self.fill_land(i, j - 1)
+        if j + 1 < len(self.grid[0]):
+            self.fill_land(i, j + 1)
         if i - 1 >= 0:
-            self.fill_land(grid, i - 1, j)
-        if i + 1 < len(grid):
-            self.fill_land(grid, i + 1, j)
+            self.fill_land(i - 1, j)
+        if i + 1 < len(self.grid):
+            self.fill_land(i + 1, j)
     
-    def find_island(self, grid, i, j):
-        while j < len(grid[0]):
-            if grid[i][j] == "1" and self.check_grid[i][j] != 1:
-                self.fill_land(grid, i, j)
+    def find_island(self, i, j):
+        while j < len(self.grid[0]):
+            if self.grid[i][j] == "1" and self.check_grid[i][j] != 1:
+                self.fill_land(i, j)
                 self.counter += 1
                 return True, (i, j)
             j += 1
         i += 1
-        while i < len(grid):
+        while i < len(self.grid):
             j = 0
-            while j < len(grid[0]):
-                if grid[i][j] == "1" and self.check_grid[i][j] != 1:
-                    self.fill_land(grid, i, j)
+            while j < len(self.grid[0]):
+                if self.grid[i][j] == "1" and self.check_grid[i][j] != 1:
+                    self.fill_land(i, j)
                     self.counter += 1
                     return True, (i, j)
                 j += 1
@@ -36,14 +36,10 @@ class Solution:
     
     def numIslands(self, grid: List[List[str]]) -> int:
         self.check_grid = [[0] * len(x) for x in grid]
+        self.grid = grid
         self.counter = 0
-        ij = (None, None)
-        while True:
-            found, ij = self.find_island(
-                grid, 
-                ij[0] if ij[0] is not None else 0,
-                ij[1] if ij[1] is not None else 0,
-            )
-            if not found:
-                break
+        found = True
+        ij = (0, 0)
+        while found:
+            found, ij = self.find_island(ij[0], ij[1])
         return self.counter

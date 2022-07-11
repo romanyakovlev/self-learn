afb481761c72f53ae3b0e636592945d478e6d1f9
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-07-12 04:01:42 +0700

    Update 200-number-of-islands.py

--- leetcode/200-number-of-islands.py
+++ leetcode/200-number-of-islands.py
@@ -1,4 +1,4 @@
-# first solution
+# first solution (dfs)
 
 class Solution:
     def fill_land(self, i, j):
@@ -43,3 +43,28 @@ class Solution:
         while found:
             found, ij = self.find_island(ij[0], ij[1])
         return self.counter
+
+
+# solution from leetcode (using dfs - cool)
+
+class Solution:
+    def numIslands(self, grid):
+        if not grid:
+            return 0
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+
+    def dfs(self, grid, i, j):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '#'
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)

c7fb0053016f2eb289287fe2d415a3d446061657
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-07-18 03:31:08 +0700

    Update 121-best-time-to-buy-and-sell-stock.py

--- leetcode/121-best-time-to-buy-and-sell-stock.py
+++ leetcode/121-best-time-to-buy-and-sell-stock.py
@@ -5,18 +5,16 @@ class Solution:
         if len(prices) == 1:
             return 0
         r = 0
-        l_stack, r_stack = [prices[0]], []
+        l_stack, r_stack = [prices[0]], [prices[-1]]
         l_min, r_max = prices[0], 0
-        i = len(prices) - 1
+        i = len(prices) - 2
         while i > 0:
-            r_max = max(r_max, prices[i])
-            r_stack.append(r_max)
+            r_stack.append(max(r_stack[-1], prices[i]))
             i -= 1
         j = 1
         while j < len(prices):
             r = max(r_stack[-1] - l_stack[-1], r)
-            l_min = min(l_min, prices[j])
-            l_stack.append(l_min)
+            l_stack.append(min(l_stack[-1], prices[j]))
             r_stack.pop()
             j += 1
         return r

f90f1b643a3baada9e7450588d2129caee80b37b
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-07-18 03:56:05 +0700

    Update 121-best-time-to-buy-and-sell-stock.py

--- leetcode/121-best-time-to-buy-and-sell-stock.py
+++ leetcode/121-best-time-to-buy-and-sell-stock.py
@@ -18,3 +18,24 @@ class Solution:
             r_stack.pop()
             j += 1
         return r
+
+# optimized first solution
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) == 1:
+            return 0
+        r = 0
+        r_stack = [prices[-1]]
+        l_min = prices[0]
+        i = len(prices) - 2
+        while i > 0:
+            r_stack.append(max(r_stack[-1], prices[i]))
+            i -= 1
+        i = 1
+        while i < len(prices):
+            r = max(r_stack[-1] - l_min, r)
+            l_min = min(l_min, prices[i])
+            r_stack.pop()
+            i += 1
+        return r

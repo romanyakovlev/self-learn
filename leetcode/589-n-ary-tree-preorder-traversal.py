28c3ae82212209a84862000d8fcbe3d3d03448a4
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-07-19 02:55:55 +0700

    Update 589-n-ary-tree-preorder-traversal.py

--- leetcode/589-n-ary-tree-preorder-traversal.py
+++ leetcode/589-n-ary-tree-preorder-traversal.py
@@ -1,10 +1,4 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val=None, children=None):
-        self.val = val
-        self.children = children
-"""
+# recursive approach
 
 class Solution:
     def preorder(self, root: 'Node') -> List[int]:

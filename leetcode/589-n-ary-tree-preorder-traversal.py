13b280f1978a86a629a0e1b22705c81fe63e32ee
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-07-19 03:10:35 +0700

    Update 589-n-ary-tree-preorder-traversal.py

--- leetcode/589-n-ary-tree-preorder-traversal.py
+++ leetcode/589-n-ary-tree-preorder-traversal.py
@@ -10,3 +10,16 @@ class Solution:
                     l.extend(self.preorder(c))
         return l
         
+# iterative approach
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        l = []
+        temp = []
+        while root:
+            l.append(root.val)
+            if root.children:
+                temp.extend(root.children[::-1])
+            root = temp.pop() if temp else None
+        return l
+        

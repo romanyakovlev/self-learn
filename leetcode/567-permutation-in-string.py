fa01a51dca751e1316f14e80d903db21a419426b
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-07-25 00:06:18 +0700

    Create 567-permutation-in-string.py

---
+++ leetcode/567-permutation-in-string.py
@@ -0,0 +1,20 @@
+# first solution
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        d_p, d = {}, {}
+        win_count = 0
+        for c in s1:
+            d_p[c] = d_p.get(c, 0) + 1
+        for i in range(len(s2)):
+            d[s2[i]] = d.get(s2[i], 0) + 1
+            win_count += 1
+            if win_count == len(s1):
+                if d == d_p:
+                    return True
+                left_win_index = i - win_count + 1
+                d[s2[left_win_index]] -= 1
+                if d[s2[left_win_index]] == 0:
+                    del d[s2[left_win_index]]
+                win_count -= 1
+        return False

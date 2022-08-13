538ac5b0d63c63be86deec94657e8793871b3fbb
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-08-14 04:18:35 +0700

    Update 15-3sum.py

--- leetcode/15-3sum.py
+++ leetcode/15-3sum.py
@@ -32,3 +32,31 @@ class Solution:
                     s.add(tuple(sorted((c1, c2, c3))))
         return [list(x) for x in s]
 
+# third solution
+
+class Solution:
+        
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        l = []
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            target = nums[i]
+            right = len(nums) - 1
+            left = i + 1
+            while left < right:
+                if nums[left] + nums[right] + target == 0:
+                    l.append([target, nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                elif -(nums[left] + nums[right]) < target:
+                    right -= 1
+                elif -(nums[left] + nums[right]) > target:
+                    left += 1
+        return l
+                

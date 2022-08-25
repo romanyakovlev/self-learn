e7d84abb86b4d6e6f372064fd4b56d516b63013c
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-08-26 01:51:10 +0700

    Update 75-sort-colors.py

--- leetcode/75-sort-colors.py
+++ leetcode/75-sort-colors.py
@@ -38,3 +38,22 @@ def sortColors(self, nums):
         else:
             nums[white], nums[blue] = nums[blue], nums[white]
             blue -= 1
+ 
+# same solution (rewritten)
+
+class Solution:
+
+    def sortColors(self, nums: List[int]) -> None:
+        i = 0
+        l, r = 0, len(nums) - 1
+        while i <= r:
+            if nums[i] == 0:
+                nums[i], nums[l] = nums[l], nums[i]
+                l += 1
+                i += 1
+            elif nums[i] == 1:
+                i += 1
+            else:
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+

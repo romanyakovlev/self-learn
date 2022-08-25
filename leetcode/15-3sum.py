a5c9ad525667ce396d4507cad374edeb88c88a26
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-08-26 02:54:51 +0700

    Update 15-3sum.py

--- leetcode/15-3sum.py
+++ leetcode/15-3sum.py
@@ -60,3 +60,55 @@ class Solution:
                     left += 1
         return l
                 
+# resolve
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            target = - nums[i]
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                if nums[l] + nums[r] == target:
+                    result.append([nums[i], nums[l], nums[r]])
+                    while l < len(nums) - 1 and nums[l] == nums[l + 1]:
+                        l += 1
+                    while r > 0 and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+                elif nums[l] + nums[r] < target:
+                    l += 1
+                else:
+                    r -= 1
+        return result
+
+# solution with explanation
+
+def threeSum(self, nums):
+    nums.sort()
+    result = []
+    for left in range(len(nums) - 2): # renamed this to left because this will always be the leftmost pointer in the triplet
+        if left > 0 and nums[left] == nums[left - 1]: # this step makes sure that we do not have any duplicates in our result output
+            continue 
+        mid = left + 1 # renamed this to mid because this is the pointer that is between the left and right pointers
+        right = len(nums) - 1
+        while mid < right:
+            curr_sum = nums[left] + nums[mid] + nums[right]
+            if curr_sum < 0:
+                mid += 1 
+            elif curr_sum > 0:
+                right -= 1
+            else:
+                result.append([nums[left], nums[mid], nums[right]])
+                while mid < right and nums[mid] == nums[mid + 1]: # Another conditional for not calculating duplicates
+                    mid += 1
+                while mid < right and nums[right] == nums[right - 1]: # Avoiding duplicates check
+                    right -= 1
+                mid += 1
+                right -= 1
+    return result

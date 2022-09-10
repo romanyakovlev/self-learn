80a397ebf10bf06927b792e23aa92ba9fe1592b3
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-09-11 01:46:03 +0700

    Update 142-linked-list-cycle-ii.py

--- leetcode/142-linked-list-cycle-ii.py
+++ leetcode/142-linked-list-cycle-ii.py
@@ -27,3 +27,37 @@ class Solution:
                 temp_node = temp_node.next
             start_node = start_node.next
         return target_node
+
+
+# second solution - hashing approach (faster)
+
+class Solution:
+    def createCycleNodeSet(self, node: ListNode) -> set[ListNode]:
+        node_set = set()
+        temp_node = node.next
+        node_set.add(temp_node)
+        while temp_node is not node:
+            temp_node = temp_node.next
+            node_set.add(temp_node)
+        return node_set
+    
+    def getFirstCycleNode(self, node: Optional[ListNode], node_set: set[ListNode]) -> ListNode:
+        while node not in node_set:
+            node = node.next
+        return node
+            
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        start_node = head
+        node1 = head
+        target_node = None
+        while head is not None and head.next is not None:
+            node1 = node1.next
+            head = head.next.next
+            if node1 is head:
+                target_node = node1
+                break
+        else:
+            return None
+        cycle_node_set = self.createCycleNodeSet(target_node)
+        start_cycle_node = self.getFirstCycleNode(start_node, cycle_node_set)
+        return start_cycle_node

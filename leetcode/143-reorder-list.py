02a456477de60e37d78f141e3d5ea06d5c830727
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-10-11 03:56:25 +0700

    Update 143-reorder-list.py

--- leetcode/143-reorder-list.py
+++ leetcode/143-reorder-list.py
@@ -1,8 +1,4 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# first solution
 
 class Solution:
     def reverseList(self, head):
@@ -33,4 +29,53 @@ class Solution:
         slow.next = None
         self.insertNodes(head, head_reversed)
         return None
-        
+
+# solution from disc
+
+class Solution:
+    
+    # Merges in place two lists
+    # @return The newly merged list.
+    def _mergeLists(self, a, b):
+        tail = head = a
+        a = a.next
+        while b:
+            tail.next = b
+            tail = tail.next
+            b = b.next
+            if a:
+                a, b = b, a
+        return head
+    
+    # Splits in place a list in two halves, the first half is >= in size than the second.
+    # @return A tuple containing the heads of the two halves
+    def _splitList(self, head):
+        fast = head
+        slow = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        middle = slow.next
+        slow.next = None
+        return head, middle
+
+    # Reverses in place a list.
+    # @return Returns the head of the new reversed list
+    def _reverseList(self, head):
+      last = None
+      currentNode = head
+      while currentNode:
+        nextNode = currentNode.next
+        currentNode.next = last
+        last = currentNode
+        currentNode = nextNode
+      return last
+
+    # @param head, a ListNode
+    # @return nothing
+    def reorderList(self, head):
+        if not head or not head.next:
+            return
+        a, b = self._splitList(head)
+        b = self._reverseList(b)
+        head = self._mergeLists(a, b)

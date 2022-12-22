f335aab3a7cdbedaca0e1734f7f7e17282295f78
Roman Yakovlev <yakovlevroman8@gmail.com>
2022-12-23 03:23:44 +0700

    Update meeting-rooms_minimum-meeting-rooms.py

--- interviewbit/meeting-rooms_minimum-meeting-rooms.py
+++ interviewbit/meeting-rooms_minimum-meeting-rooms.py
@@ -6,16 +6,18 @@ class Solution:
     # @return an integer
     def solve(self, A):
         A.sort(key=lambda x: (x[0], x[1]), reverse=True)
-        h = []
-        c = 1
+        heap = []
+        counter = 1
         while A:
-            m = A.pop()
-            while h:
-                x = (h[0][1], h[0][0])
-                if x[1] <= m[0]:
-                    heappop(h)
+            room = A.pop()
+            while heap:
+                # swap end and start again (from string #20)
+                room_with_min_end_from_heap = (heap[0][1], heap[0][0])
+                if room_with_min_end_from_heap[1] <= room[0]:
+                    heappop(heap)
                 else:
                     break
-            heappush(h, (m[1], m[0]))
-            c = max(c, len(h))
-        return c
+            # swap end and start to build heap properly
+            heappush(heap, (room[1], room[0]))
+            counter = max(counter, len(heap))
+        return counter

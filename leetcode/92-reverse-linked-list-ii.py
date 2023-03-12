1dfcdbcca73633373af0f838492b107cbbd7f51a
Roman Yakovlev <yakovlevroman8@gmail.com>
2023-03-13 02:54:03 +0700

    Update 92-reverse-linked-list-ii.py

--- leetcode/92-reverse-linked-list-ii.py
+++ leetcode/92-reverse-linked-list-ii.py
@@ -82,3 +82,19 @@ class Solution:
             temp.next = pre.next
             pre.next = temp
         return dummy.next
+ 
+# reviced solution №1
+
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy = prev = ListNode(0)
+        dummy.next = head
+        for _ in range(left - 1):
+            prev = head
+            head = head.next
+        for _ in range(right - left):
+            temp = head.next
+            head.next = temp.next
+            temp.next = prev.next
+            prev.next = temp
+        return dummy.next

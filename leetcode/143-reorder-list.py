2cac3a504ef971880e37fbf4c451b9e1f3fe5fb1
Roman Yakovlev <yakovlevroman8@gmail.com>
2023-03-14 16:00:16 +0700

    Update 143-reorder-list.py

--- leetcode/143-reorder-list.py
+++ leetcode/143-reorder-list.py
@@ -98,3 +98,31 @@ class Solution:
             cur1, cur2 = head.next, head_reversed.next
             head.next, head_reversed.next = head_reversed, cur1
             head, head_reversed = cur1, cur2
+
+# from disc
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head:
+            return
+        # find the middle of linked list [Problem 876]
+        # in 1->2->3->4->5->6 find 4 
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        # reverse the second part of the list [Problem 206]
+        # convert 1->2->3->4->5->6 into 1->2->3->4 and 6->5->4
+        # reverse the second half in-place
+        prev, curr = None, slow
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
+        # merge two sorted linked lists [Problem 21]
+        # merge 1->2->3->4 and 6->5->4 into 1->6->2->5->3->4
+        first, second = head, prev
+        while second.next:
+            first.next, first = second, first.next
+            second.next, second = first, second.next

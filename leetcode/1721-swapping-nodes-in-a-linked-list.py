33e3ab6ce282432387a947fad98493d50313ddbf
Roman Yakovlev <yakovlevroman8@gmail.com>
2023-03-30 03:32:18 +0700

    Update 1721-swapping-nodes-in-a-linked-list.py

--- leetcode/1721-swapping-nodes-in-a-linked-list.py
+++ leetcode/1721-swapping-nodes-in-a-linked-list.py
@@ -25,3 +25,26 @@ class Solution:
         else:
             cur2.next, cur1.next, prev2.next = cur1.next, cur2.next, cur1
         return dummy.next
+
+# solution from disc
+
+def swapNodes(self, head: ListNode, k: int) -> ListNode:
+    dummy = pre_right = pre_left = ListNode(next=head)
+    right = left = head
+    for i in range(k-1):
+        pre_left = left
+        left = left.next
+    
+    null_checker = left
+    
+    while null_checker.next:
+        pre_right = right
+        right = right.next
+        null_checker = null_checker.next
+        
+    if left == right:
+        return head
+    
+    pre_left.next, pre_right.next = right, left
+    left.next, right.next = right.next, left.next
+    return dummy.next
